f7e48cee28f4505046a215f9d1443981ed6e1917
= <=>
2025-05-01 11:55:57 +0200

    move zed settings to `home.nix`

--- hosts/default/home.nix
+++ hosts/default/home.nix
@@ -104,6 +104,66 @@
   
   programs.zed-editor = {
     enable = true;
+    extraPackages = with pkgs; [ nixd nil ];
+    userSettings = {
+      show_edit_predictions = true;
+      features = {
+        edit_prediction_provider = "zed";
+      };
+      base_keymap = "VSCode";
+      ui_font_size = 16;
+      buffer_font_size = 15.0;
+      # buffer_font_weight = 100;
+      ui_font_family = "Noto Sans";
+      buffer_font_family = "Noto Sans Mono";
+      theme = {
+          mode = "system";
+          light = "One Light";
+          dark = "Gruvbox Dark Hard";
+      };
+      autosave = {
+        after_delay = {
+          milliseconds = 1000;
+        };
+      };
+      wrap_guides = [100];
+      preferred_line_length = 100;
+      soft_wrap = "preferred_line_length";
+      file_types = {
+        c = ["c" "h"];
+        Assembly = ["*.casm"];
+      };
+      languages = {
+        Assembly = {
+          show_edit_predictions = false;
+        };
+        Markdown = {
+          tab_size = 4;
+        };
+      };
+    };
+    userKeymaps = [
+      {
+        context = "Workspace";
+        bindings = {
+          "ctrl-`" = "workspace::ToggleBottomDock";
+          "ctrl-r" = "projects::OpenRecent";
+          # "ctrl-:" = "editor::ToggleInlayHints";
+        };
+      }
+      {
+        context = "Terminal";
+        bindings = {
+          "ctrl+t" = "workspace::NewTerminal";
+        };
+      }
+      {
+        context = "Editor";
+        bindings = {
+          # "j k": ["workspace::SendKeystrokes", "escape"]
+        };
+      }
+    ];
   };
   
   programs.zed-editor-extensions = {
