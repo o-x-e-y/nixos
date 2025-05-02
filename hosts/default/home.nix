22a89ce99590d8425a69a6587866e00ae146375a
= <=>
2025-05-03 00:04:24 +0200

    add btop

--- hosts/default/home.nix
+++ hosts/default/home.nix
@@ -21,6 +21,7 @@
     discord
     spotify
     vscode
+    btop
 
     # # It is sometimes useful to fine-tune packages, for example, by applying
     # # overrides. You can do that directly here, just don't forget the
