587cd19f65e8d893b80a73334384170225db52af
oxey <lucoerlemans37@gmail.com>
2025-05-05 18:46:06 +0200

    actually add tinymist as a package

--- hosts/default/home.nix
+++ hosts/default/home.nix
@@ -27,6 +27,7 @@ in
     btop
     typst
     typstyle
+    tinymist
 
     # # It is sometimes useful to fine-tune packages, for example, by applying
     # # overrides. You can do that directly here, just don't forget the
