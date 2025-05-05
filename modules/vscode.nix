67f0174aef09320bf57f1409bcdad2a0d481afc1
= <=>
2025-05-05 13:21:39 +0200

    fix vscode extensions

--- modules/vscode.nix
+++ modules/vscode.nix
@@ -4,35 +4,48 @@
     programs.vscode = {
       enable = true;
       package = pkgs.vscode;
-      extensions = with pkgs.vscode-extensions; [
-        harryhopkinson.vim-theme
-        jonathanharty.gruvbox-material-icon-theme
-        mfederczuk.w3c-ebnf
-        ritwickdey.liveserver
-        tamasfe.even-better-toml
-        mechatroner.rainbow-csv
-        davidanson.vscode-markdownlint
-        kamikillerto.vscode-colorize
-        myriad-dreamin.tinymist
-        # bradlc.vscode-tailwindcss
-        # austenc.tailwind-docs
-        # bourhaouta.tailwindshades
-        # huber-baste.msgpack
-        # github.vscode-github-actions
-        # ms-vscode.makefile-tools
-        # charliermarsh.ruff
-        # ms-python.python
-        # qwtel.sqlite-viewer
-        # rust-lang.rust-analyzer
-      ];
-      userSettings = {
-        "git.autofetch" = true;
-        "files.autoSave" = "afterDelay";
-        "editor.fontSize" = 14;
-        "debug.console.fontSize" = 14;
-        "terminal.integrated.fontSize" = 14;
-        "window.zoomLevel" = 1;
-        "[nix]"."editor.tabSize" = 2;
+      profiles.default = {
+        extensions = with pkgs.vscode-extensions; [
+          ritwickdey.liveserver
+          tamasfe.even-better-toml
+          mechatroner.rainbow-csv
+          davidanson.vscode-markdownlint
+          kamikillerto.vscode-colorize
+          myriad-dreamin.tinymist
+          
+          # bradlc.vscode-tailwindcss
+          # austenc.tailwind-docs
+          # bourhaouta.tailwindshades
+          # huber-baste.msgpack
+          # github.vscode-github-actions
+          # ms-vscode.makefile-tools
+          # charliermarsh.ruff
+          # ms-python.python
+          # qwtel.sqlite-viewer
+          # rust-lang.rust-analyzer
+        ] ++ pkgs.vscode-utils.extensionsFromVscodeMarketplace [
+          {
+            name = "vim-theme";
+            publisher = "harryhopkinson";
+            version = "1.1.8";
+            sha256 = "sha256-W/OaCEhMbBkmtqIezTs0DJbZQyUVMshY3XiPPHcgf7Y=";
+          }
+          {
+            name = "gruvbox-material-icon-theme";
+            publisher = "jonathanharty";
+            version = "1.1.5";
+            sha256 = "sha256-86UWUuWKT6adx4hw4OJw3cSZxWZKLH4uLTO+Ssg75gY=";
+          }
+        ];
+        userSettings = {
+          "git.autofetch" = true;
+          "files.autoSave" = "afterDelay";
+          "editor.fontSize" = 14;
+          "debug.console.fontSize" = 14;
+          "terminal.integrated.fontSize" = 14;
+          "window.zoomLevel" = 1;
+          "[nix]"."editor.tabSize" = 2;
+        };
       };
     };
   };
