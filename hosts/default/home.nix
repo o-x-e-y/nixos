35b9e4ddc08c614364561acf9e00f0bbb9dfc6e0
oxey <lucoerlemans37@gmail.com>
2025-05-08 14:30:16 +0200

    remove superfluous `alacritty-theme`

--- hosts/default/home.nix
+++ hosts/default/home.nix
@@ -33,7 +33,6 @@ in
     fastfetch
     ungoogled-chromium
     alacritty
-    alacritty-theme
 
     # # It is sometimes useful to fine-tune packages, for example, by applying
     # # overrides. You can do that directly here, just don't forget the
