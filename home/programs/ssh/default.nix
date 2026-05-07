cf61cdc6993460288e4af5b62f14e4b436aa9113
oxey <lucoerlemans37@gmail.com>
2026-05-07 18:30:05 +0200

    disable default ssh config as advised

--- home/programs/ssh/default.nix
+++ home/programs/ssh/default.nix
@@ -18,6 +18,7 @@ in
   config = lib.mkIf cfg.enable {
     programs.ssh = {
       enable = true;
+      enableDefaultConfig = false;
       matchBlocks."github.com" = {
         hostname = "github.com";
         user = "git";
