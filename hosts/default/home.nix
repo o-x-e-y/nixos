9e8a523fa219e3128f4eb1ffb9fd2bbbeaa8dac7
oxey <lucoerlemans37@gmail.com>
2026-06-22 09:17:47 +0200

    uninstall flips

--- hosts/default/home.nix
+++ hosts/default/home.nix
@@ -77,7 +77,6 @@ in
     claude-monitor
     fend
     oxeylyzer
-    flips
 
     # # It is sometimes useful to fine-tune packages, for example, by applying
     # # overrides. You can do that directly here, just don't forget the
