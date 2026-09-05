670660a7576f106dba9b1bf01d4b528158286f5b
oxey <lucoerlemans37@gmail.com>
2026-09-05 13:05:49 +0200

    temporarily update claude-code to v2.1.258 for fable 5.1

--- flake.nix
+++ flake.nix
@@ -67,9 +67,9 @@
             };
 
             claudeCodeOverride = {
-              enable = false;
-              version = "2.1.170";
-              hash = "sha256-hJ4AcnegRCqydXDT49bUN4dQeUZZDo3RlH5aObcIH54=";
+              enable = true;
+              version = "2.1.258";
+              hash = "sha256-cE8TNKxl0+ieHGwddmMpOteGphZq/bcbUHUzffYw+XY=";
             };
 
             nixpkgs.overlays = [
