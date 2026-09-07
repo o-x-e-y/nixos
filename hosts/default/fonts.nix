6b953a7f1301d1437c71247f189bf10d717f77ca
oxey <lucoerlemans37@gmail.com>
2026-09-07 15:09:18 +0200

    remove custom code for generating static width fonts, which isn't necessary for typst anymore

--- hosts/default/fonts.nix
+++ hosts/default/fonts.nix
@@ -2,40 +2,6 @@
   pkgs,
   ...
 }:
-let
-  font-to-static = pkgs.writeShellApplication {
-    name = "font-to-static";
-    runtimeInputs = [ pkgs.python3Packages.fonttools ];
-    text = ''
-      src="$1"
-      name="$(basename "$src" '.ttf')"
-      for wght in 100 200 300 400 500 600 700 800 900; do
-        fonttools varLib.mutator -o "$name-$wght.ttf" "$src" wght="$wght"
-      done
-    '';
-  };
-
-  fonts-static =
-    (pkgs.google-fonts.override {
-      fonts = [
-        "Yrsa"
-        "DM Sans"
-        "Cascadia Code"
-        "Inter"
-        "IBM Plex Sans"
-        "Source Serif 4"
-      ];
-    }).overrideAttrs
-      (
-        final: prev: {
-          nativeBuildInputs = (prev.nativeBuildInputs or [ ]) ++ [ font-to-static ];
-
-          preFixup = ''
-            find "$out" -name '*.ttf' -execdir font-to-static '{}' ';'
-          '';
-        }
-      );
-in
 {
   fonts.packages = with pkgs; [
     noto-fonts
@@ -44,6 +10,19 @@ in
     texlivePackages.inter
     courier-prime
     roboto
-    fonts-static
+
+    # These six ship from Google as variable fonts only. Typst reads the
+    # wght/wdth/opsz axes directly since 0.15, so they no longer need to be
+    # instantiated into static weights first.
+    (google-fonts.override {
+      fonts = [
+        "Yrsa"
+        "DM Sans"
+        "Cascadia Code"
+        "Inter"
+        "IBM Plex Sans"
+        "Source Serif 4"
+      ];
+    })
   ];
 }
