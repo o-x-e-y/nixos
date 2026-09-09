2c96cd1c176b0e36578bd7e39a5a46a31ef399e4
oxey <lucoerlemans37@gmail.com>
2026-09-09 11:38:22 +0200

    install google-chrome for claude-in-chrome

--- hosts/default/home.nix
+++ hosts/default/home.nix
@@ -80,6 +80,7 @@ in
     fend
     oxeylyzer
     wheelwizard
+    google-chrome
 
     # # It is sometimes useful to fine-tune packages, for example, by applying
     # # overrides. You can do that directly here, just don't forget the
